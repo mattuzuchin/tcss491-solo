b928bd6f5f2dca90caf0684cbe7eed7c3fe3bbd5
mattuzuchin <mu26@uw.edu>
2025-01-19 22:11:06 -0800

    phase 1

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./runner.png");
+ASSET_MANAGER.queueDownload("./file.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
