32682d96f74ac3556cdb14ba9df7e8c9f124b76c
mattuzuchin <mu26@uw.edu>
2025-01-19 21:45:36 -0800

    phase 1

--- luke.js
+++ luke.js
@@ -1,7 +1,7 @@
 class Luke {
 	constructor(game) {
 		this.game = game;
-		this.animator = new Animator(ASSET_MANAGER.getAsset("./cheetah.png"), 2, 0, 500, 214, 3, 0.2);
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./cheetah.png"), 2, 0, 500, 214, 4, 0.2);
 	};
 
 	update() {
