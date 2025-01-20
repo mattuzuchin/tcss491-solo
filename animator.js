55c9ed1361866b3bd01ac939678f76fcde82e76f
mattuzuchin <mu26@uw.edu>
2025-01-19 21:43:48 -0800

    phase 1

--- animator.js
+++ animator.js
@@ -14,7 +14,7 @@ class Animator {
 			this.xStart + this.width * frame,this.yStart,
 			this.width, this.height,
 			x,y,
-			this.width*5, this.height*5);
+			this.width, this.height);
 	};
 
 	currentFrame() {
--- luke.js
+++ luke.js
@@ -1,7 +1,7 @@
 class Luke {
 	constructor(game) {
 		this.game = game;
-		this.animator = new Animator(ASSET_MANAGER.getAsset("./walker.png"), 2, 0, 61, 70, 3, 0.2);
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./cheetah.png"), 2, 0, 500, 214, 3, 0.2);
 	};
 
 	update() {
